cbe1d726e557efc16e8b2910e8082f087ee9d4ef
Neeraj7015187103 <136111107+Neeraj7015187103@users.noreply.github.com>
2023-07-25 21:02:36 +0530

    Add files via upload

---
+++ SQL ASSIGNMENT 1.sql
@@ -0,0 +1,23 @@
+### Assignemnt 
+
+USE DAY1
+
+CREATE table store_sales
+(month varchar(20) not null,
+prod_name varchar(20) not null,
+sales int not null);
+
+
+ select * from store_sales;
+ 
+ INSERT into store_sales 
+values ("Jan","Fruit",45000),
+ ("Jan","Vegetables",67000),
+ ("Jan","Dairy",55000),
+ ("Feb","Fruit",42000),
+ ("Feb","Vegetables",32000),
+ ("Feb","Dairy",52000),
+ ("March","Fruit",61000),
+ ("March","Vegetables",43000),
+ ("March","Dairy",92000);
+ 
